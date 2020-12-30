c92bc75fdd62d3f687d02a0d6a946177fdbe6ce3
svatwork <sebastiaan.verbeek@redkubes.com>
2020-12-30 09:44:16 +0100

    fix: maybe easier to just assign the mock-schema

--- src/tasks/migrate-values/migrate-values.test.ts
+++ src/tasks/migrate-values/migrate-values.test.ts
@@ -1,22 +1,43 @@
-import { assert } from 'chai'
+import { assert, expect } from 'chai'
 import fs from 'fs'
 import path from 'path'
 import yaml from 'js-yaml'
 
-const revisionSchemaKeys: string[] = [
-  'REV',
-  'displacements',
-  'deletions',
-  'forwardMutations',
-  'backwardMutations',
-  'version',
+const mock = [
+  {
+    REV: '8665e5707c829eaff674bd416de8610a2103114a',
+    displacements: {
+      'charts.president.lastName': 'charts.cert-manager.president.lastName',
+    },
+    deletions: {
+      'charts.president.firstName': 'charts.cert-manager.president.firstName',
+    },
+    forwardMutations: { 'charts.cert-manager.president.lastName': 'printf "v%s"' },
+    backwardMutations: { 'charts.cert-manager.president.lastName': 'replace "v" ""' },
+    version: '0.2.0',
+  },
+  {
+    REV: '03f5c491617cc8d4dc1c237c161ac50e90eeac02',
+    displacements: {
+      'charts.president.lastName': 'charts.cert-manager.president.lastName',
+    },
+    deletions: {
+      'charts.president.firstName': 'charts.cert-manager.president.firstName',
+    },
+    forwardMutations: { 'charts.cert-manager.president.lastName': 'printf "v%s"' },
+    backwardMutations: { 'charts.cert-manager.president.lastName': 'replace "v" ""' },
+  },
+  {
+    REV: '4a64c83c0991269f9f64c4a7afcb08e3c2435c4f',
+    displacements: null,
+    deletions: null,
+    forwardMutations: null,
+    backwardMutations: null,
+  },
 ]
 
-const revisionPath = fs.readFileSync(path.join(__dirname, 'revision.yaml'), 'utf8')
-
 describe('YAML parsing', () => {
-  it('should have one of the revisionSchemaKeys', (done) => {
-    console.log(yaml.safeLoadAll(revisionPath))
+  it('should verify each revision with the hyper-schema', (done) => {
     done()
   })
 })
