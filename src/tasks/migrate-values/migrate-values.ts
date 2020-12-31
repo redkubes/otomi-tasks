e386241a4274d1e5515b0d94c55793d579c58c75
svatwork <sebastiaan.verbeek@redkubes.com>
2020-12-31 12:59:07 +0100

    feat(tdd): green hat, otomi-values checking

--- src/tasks/migrate-values/migrate-values.ts
+++ src/tasks/migrate-values/migrate-values.ts
@@ -1,12 +1,21 @@
-import fs from 'fs'
+import { readdirSync } from 'fs'
 import path from 'path'
 import yaml from 'js-yaml'
 
 export function readOtomiValuesDir(path: string): object {
-  return { env: 'charts' }
+  const files: string[] = []
+  const envPath = path + '/env'
+  try {
+    readdirSync(envPath).forEach((file) => files.push(file))
+    console.log(files)
+    return { env: files }
+  } catch (error) {
+    console.error('Not an otomi-values directory: ' + error)
+    return {}
+  }
 }
 
-export function listEnvDirectory(envDir: object): object {
+export function listEnvDirectory(envDirContents: object): object {
   return {
     'otomi-values': {
       env: ['charts', 'clouds', 'teams'],
--- src/tasks/migrate-values/test/migrate-values.test.ts
+++ src/tasks/migrate-values/test/migrate-values.test.ts
@@ -3,28 +3,47 @@ import fs from 'fs'
 import path from 'path'
 import yaml from 'js-yaml'
 import * as mv from '../migrate-values'
-import { has, isEqual } from 'lodash'
+import { get, isEmpty, isEqual } from 'lodash'
 
 const mockIncomingChangesFileName = 'mock-incoming-changes.yaml'
 const mockIncomingChanges = yaml.safeLoadAll(
   fs.readFileSync(path.join(__dirname, mockIncomingChangesFileName), 'utf-8'),
 )
 
-describe('TDD test suite', () => {
-  it('can find something that looks like an otomi-values directory', () => {
-    const testPath = path.join(__dirname, 'otomi-values')
-    assert(has(mv.readOtomiValuesDir(testPath), 'env'), "the built dictionary doesn't have this key 'env'")
+describe('migrate-values.ts', () => {
+  describe('readOtomiValuesDir()', () => {
+    it('can find something that looks like an otomi-values directory', () => {
+      const testPath = path.join(__dirname, 'otomi-values')
+      assert(get(mv.readOtomiValuesDir(testPath), 'env', 'charts') !== undefined)
+    })
+    it('should return {} with an invalid path', () => {
+      const wrongPath = 'invalid-path'
+      assert.throws(function () {
+        if (isEmpty(mv.readOtomiValuesDir(wrongPath))) {
+          throw new Error()
+        }
+      }, Error)
+    })
+    it('should return {} with an empty path', () => {
+      const wrongPath = ''
+      assert.throws(function () {
+        if (isEmpty(mv.readOtomiValuesDir(wrongPath))) {
+          throw new Error()
+        }
+      }, Error)
+    })
   })
-
-  it('can read the test directory otomi-values', () => {
-    const envDirPath = mv.readOtomiValuesDir('otomi-values')
-    assert(
-      isEqual(mv.listEnvDirectory(envDirPath), {
-        'otomi-values': {
-          env: ['charts', 'clouds', 'teams'],
-        },
-      }),
-      "listEnvDirectory doesn't return the contents of env",
-    )
+  describe('listEnvDirectory()', () => {
+    it('can read the test directory otomi-values', () => {
+      const envDirPath = mv.readOtomiValuesDir(path.join(__dirname, 'otomi-values'))
+      assert(
+        isEqual(mv.listEnvDirectory(envDirPath), {
+          'otomi-values': {
+            env: ['charts', 'clouds', 'teams'],
+          },
+        }),
+        "listEnvDirectory doesn't return the contents of env",
+      )
+    })
   })
 })
