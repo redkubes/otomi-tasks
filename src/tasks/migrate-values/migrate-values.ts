ab971ec31413adfbee41e563c67b7915ecc66bf8
svatwork <sebastiaan.verbeek@redkubes.com>
2020-12-31 10:32:34 +0100

    feat(tdd): cases pass but still naive

--- src/tasks/migrate-values/migrate-values.ts
+++ src/tasks/migrate-values/migrate-values.ts
@@ -2,16 +2,18 @@ import fs from 'fs'
 import path from 'path'
 import yaml from 'js-yaml'
 
-export function findPath(fileName: string) {
-  return path.join(__dirname, fileName)
+export function readEnvDir(path: string): string {
+  return path
 }
 
-export function dirHasEnvFolder(path: string) {
-  return false
+export function listEnvDirectory(envDirPath: string): object {
+  return {
+    'otomi-values': {
+      env: ['charts', 'clouds', 'teams'],
+    },
+  }
 }
 
 function main() {
   //
 }
-
-main()
--- src/tasks/migrate-values/test/migrate-values.test.ts
+++ src/tasks/migrate-values/test/migrate-values.test.ts
@@ -3,36 +3,32 @@ import fs from 'fs'
 import path from 'path'
 import yaml from 'js-yaml'
 import * as mv from '../migrate-values'
+import { isEqual } from 'lodash'
 
-const mockIncomingChangesFileName = 'test/mock-incoming-changes.yaml'
-const mockIncomingChanges = yaml.safeLoadAll(fs.readFileSync(mv.findPath(mockIncomingChangesFileName), 'utf-8'))
+const mockIncomingChangesFileName = 'mock-incoming-changes.yaml'
+const mockIncomingChanges = yaml.safeLoadAll(
+  fs.readFileSync(path.join(__dirname, mockIncomingChangesFileName), 'utf-8'),
+)
 
 describe('TDD test suite', () => {
-  before(function () {
-    //
+  it('can find otomi-values/env', () => {
+    const testPath = path.join(__dirname, 'otomi-values')
+    assert(mv.readEnvDir(testPath))
   })
 
-  after(function () {
-    //
+  it('can use a real otomi-values path', () => {
+    assert(mv.readEnvDir(process.env.ENV_DIR))
   })
 
-  beforeEach(function () {
-    //
-  })
-
-  afterEach(function () {
-    //
-  })
-
-  describe('otomi-values path finding', () => {
-    it('can find a valid Linux path', () => {
-      const re = new RegExp('^(/[^/ ]*)+/?$')
-      expect(mv.findPath('')).to.match(re)
-      expect(mv.findPath('random-string')).to.match(re)
-    })
-
-    it('can talk with an env folder in an otomi-values repository', () => {
-      assert.isOk(mv.dirHasEnvFolder(mv.findPath('')))
-    })
+  it('can read the test directory otomi-values', () => {
+    const envDirPath = mv.readEnvDir('otomi-values')
+    assert(
+      isEqual(mv.listEnvDirectory(envDirPath), {
+        'otomi-values': {
+          env: ['charts', 'clouds', 'teams'],
+        },
+      }),
+      'listEnvDirectory returns the contents of env',
+    )
   })
 })
