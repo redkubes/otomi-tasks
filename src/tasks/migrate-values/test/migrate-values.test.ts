08d022327875fb7e02a609fc0b360cd9f6297420
svatwork <sebastiaan.verbeek@redkubes.com>
2021-01-05 12:24:14 +0100

    feat: good enough to test otomi-values presence

--- src/tasks/migrate-values/test/migrate-values.test.ts
+++ src/tasks/migrate-values/test/migrate-values.test.ts
@@ -2,6 +2,7 @@ import { assert, expect } from 'chai'
 import fs from 'fs'
 import path from 'path'
 import yaml from 'js-yaml'
+import * as mv from '../migrate-values'
 import { get, isEmpty, isEqual, isMatch } from 'lodash'
 import { glob } from 'glob'
 
@@ -11,12 +12,22 @@ const mockIncomingChanges = yaml.safeLoadAll(
 )
 
 describe('migrate-values.ts', () => {
-  describe('globYAML()s', () => {
-    const options = {
-      cwd: testPath,
-    }
-    glob('**/*.ya*ml', options, (error, files) => {
-      console.log(files)
+  const testPath: string = __dirname + '/otomi-values/esnv'
+  if (!fs.existsSync(testPath)) {
+    throw new Error(`
+    'otomi-values' test-directory is not present. 
+    This check prevents testing without a sample 'otomi-values' directory.
+    This test is pretty meaningless without it.
+
+    expected path: ${__dirname + '/otomi-values/env'}
+    actual path: ${testPath}
+    `)
+  }
+
+  describe('globWrapper()', () => {
+    it('only works with /env sub-path', () => {
+      console.log(testPath)
+      // assert.equal(globWrapper())
     })
   })
 })
