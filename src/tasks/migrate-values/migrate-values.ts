783d4276740baf8d6b8882c1204118315152dfc4
svatwork <sebastiaan.verbeek@redkubes.com>
2021-01-07 09:58:28 +0100

    feat(tdd-blue): file name returns json object

--- src/tasks/migrate-values/migrate-values.ts
+++ src/tasks/migrate-values/migrate-values.ts
@@ -1,18 +1,24 @@
 import glob from 'glob'
 import yaml from 'js-yaml'
+import fs from 'fs'
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
 export function globWrapper(path: string, cb?) {
   if (!path.includes('/env')) {
     throw new Error('Does not contain env substring')
   }
   if (cb) {
     glob('**/*.yaml', { cwd: path }, (error, files: string[]) => {
-      cb(files)
+      cb(files.map((f) => path + '/' + f))
     })
   }
 }
 
-export function otomiValuesLoader(pathList: string) {
-  return {}
+export function otomiValuesLoader(pathList: string[], fileName: string) {
+  const re = new RegExp(`/env/${fileName}$`)
+  return yaml.load(
+    fs.readFileSync(
+      pathList.find((element) => re.exec(element)),
+      'utf-8',
+    ),
+  )
 }
--- src/tasks/migrate-values/test/migrate-values.test.ts
+++ src/tasks/migrate-values/test/migrate-values.test.ts
@@ -42,12 +42,82 @@ describe('migrate-values.ts', () => {
     })
   })
   describe('otomiValuesLoader()', () => {
-    let testPathList = ''
-    mv.globWrapper(testPath, (files) => {
+    let testPathList: string[] = []
+    mv.globWrapper(testPath, (files: string[]) => {
       testPathList = files
     })
-    it('parses otomi-values/env and returns an object', () => {
-      assert.isObject(mv.otomiValuesLoader(testPathList))
+    it('can take a file name and return the json object', () => {
+      assert.deepEqual(mv.otomiValuesLoader(testPathList, 'teams.yaml'), {
+        teamConfig: {
+          teams: {
+            demo: {
+              alerts: {
+                receivers: ['email'],
+              },
+              id: 'demo',
+              clusters: ['aws/demo', 'azure/demo', 'google/demo', 'onprem/demo'],
+            },
+          },
+        },
+      })
+      assert.deepEqual(mv.otomiValuesLoader(testPathList, 'clusters.yaml'), {
+        clouds: {
+          aws: {
+            domain: 'eks.otomi.cloud',
+            clusters: {
+              demo: {
+                enabled: false,
+                apiName: 'eks_otomi-cloud_eu-central-1_otomi-eks-demo',
+                apiServer: '1.1.1.1',
+                k8sVersion: '1.17',
+                otomiVersion: 'master',
+                region: 'eu-central-1',
+              },
+            },
+          },
+          google: {
+            domain: 'gke.otomi.cloud',
+            clusters: {
+              demo: {
+                enabled: true,
+                apiName: 'gke_otomi-cloud_europe-west4_otomi-gke-demo',
+                apiServer: '1.1.1.1',
+                k8sVersion: '1.18',
+                otomiVersion: 'master',
+                region: 'europe-west4',
+              },
+            },
+          },
+          azure: {
+            domain: 'aks.otomi.cloud',
+            clusters: {
+              demo: {
+                enabled: false,
+                apiName: 'aks_otomi-cloud_westeurope_otomi-aks-demo',
+                apiServer: '1.1.1.1',
+                k8sVersion: '1.18',
+                otomiVersion: 'master',
+                region: 'westeurope',
+              },
+            },
+          },
+          onprem: {
+            domain: 'onprem.otomi.cloud',
+            clusters: {
+              demo: {
+                enabled: false,
+                dnsProvider: 'google',
+                apiName: 'onprem_otomi-cloud',
+                apiServer: '1.1.1.1',
+                k8sVersion: '1.19',
+                otomiVersion: 'master',
+                region: 'local',
+                entrypoint: '1.1.1.1',
+              },
+            },
+          },
+        },
+      })
     })
   })
 })
