a518986d4bb7cac1c380362b3950e08da11b5f67
svatwork <sebastiaan.verbeek@redkubes.com>
2021-01-18 14:10:51 +0100

    feat: add dependencies

--- src/tasks/migrate-values/index.ts
+++ src/tasks/migrate-values/index.ts
@@ -2,16 +2,18 @@ import path from 'path'
 import minimist from 'minimist'
 import * as mv from './migrate-values'
 import yaml from 'js-yaml'
+import fs from 'fs'
 
 const argv = minimist(process.argv.slice(2))
-if ('env-dir' in argv) {
+console.log(argv)
+if ('env-dir' in argv && 'file' in argv && 'changes' in argv && 'op' in argv) {
   mv.globWrapper(argv['env-dir'], (files) => {
     const param = {
-      otomiValues: mv.otomiValuesLoader(files, 'jobs.demo.yaml'),
-      changes: {},
+      otomiValues: mv.otomiValuesLoader(files, argv['file']),
+      changes: yaml.safeLoadAll(fs.readFileSync(path.resolve(argv['changes']), 'utf-8'))[0],
       oldVersion: mv.getOldVersion(),
       newVersion: mv.getNewVersion(),
-      operation: 'displacements',
+      operation: argv['op'],
     }
 
     const change = mv.migrateValues(
